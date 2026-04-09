c8d3dd600eda11b02293ba2cffdb3d063b319582
effsolplaatjie-ux <effsolplaatjie@gmail.com>
2026-04-09 14:33:27 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const twilio = require('twilio');
+const PDFDocument = require('pdfkit'); // <--- ENSURE THIS LINE IS HERE
 
 const app = express();
 
